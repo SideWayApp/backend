9228c14cd7f6a5f6d732da945664a19cae0ecf73
Yuvalkop22 <Kopelman050@gmail.com>
2023-03-04 13:26:54 +0200

    directions api changes

---
+++ Controllers/directionsContorller.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+exports.getDirections = async (req, res) => {
+  const { origin, destination, mode, alternatives } = req.query;
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const url = 'https://maps.googleapis.com/maps/api/directions/json';
+  const params = {
+    origin: origin,
+    destination: destination,
+    mode: mode || 'walking',
+    alternatives: alternatives || true,
+    key: apiKey
+  };
+
+  try {
+    const response = await axios.post(url, params);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
+const directionsRoute = require('./Routes/directionsRoutes ');
 const app = express();
 const PORT = 8080;
-
+app.use('/api', directionsRoute);
 app.listen(PORT,console.log(`port is running on port ${PORT}...`));
