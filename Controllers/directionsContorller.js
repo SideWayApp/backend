334f752898ec58b18ef0915f928717d1a58ccf5c
Yuvalkop22 <Kopelman050@gmail.com>
2023-03-05 08:41:26 +0200

    get streets

--- Controllers/directionsContorller.js
+++ Controllers/directionsContorller.js
@@ -3,18 +3,32 @@ require('dotenv').config();
 
 const apiKey = process.env.GOOGLE_MAPS_API_KEY;
 exports.getDirections = async (req, res) => {
- // const { origin, destination, mode, alternatives } = req.query;
+ const { origin, destination} = req.body;
+  console.log(origin + " , " + destination)
     const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-    const url = 'https://maps.googleapis.com/maps/api/directions/json';
-    const origin= "Ha-Helmonit 34,Rishon Le-Zion";
-    const destination= "Eli Vizel 2, Rishon Le-Zion";
     const mode= 'walking';
     const alternatives= true;
     const apiUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}&mode=${mode}&alternatives=${alternatives}`;
 
   try {
     const response = await axios.get(apiUrl);
-    res.json(response.data);
+    const ret = response.data;
+    let streets = new Set();
+    ret.routes.map(route=>{
+    route.legs.map(leg => {
+        leg.steps.map(step=>{
+            streets.add(step.html_instructions);
+        })
+      })
+    });
+    const streetsArray = Array.from(streets);
+    const streetRegex = /<b>([^<]+)\b\s*St<\/b>/gi;
+
+    const streetMatches = streetsArray.join(',').match(streetRegex);
+    const streets1 = streetMatches.map(match => match.replace(/<\/?b>/g, ''));
+
+    console.log(streets1);
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
