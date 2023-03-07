4caa8f895a3f6de64555a6984bd42a5a8a4f5884
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-03-07 14:13:10 +0200

    added gis scraper

--- Controllers/scraperController.js
+++ Controllers/scraperController.js
@@ -4,14 +4,16 @@ const cheerio = require("cheerio");
 const scrapeStreetsData = async (url) => {
   try {
     const response = await axios.get(url);
-    const html = response.data;
     const $ = cheerio.load(response.data);
     const streets = [];
-    $(`.col.py-1`, html).each((i, el) => {
-      console.log(el);
+    $(`.col.py-1`).each((i, el) => {
       const street = {
         name: $(el).text().trim(),
-        url: $(el).attr("href"),
+        clean: 1,
+        safe: 1,
+        scenery: 1,
+        accessible: 1,
+        total: 1,
       };
       streets.push(street);
     });
@@ -29,6 +31,28 @@ exports.getScarpedStreets = async (req, res) => {
     );
     console.log(streets);
     res.send(streets);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getStreetsFromGis = async (req, res) => {
+  try {
+    const url =
+      "https://gisn.tel-aviv.gov.il/GisOpenData/service.asmx/GetLayer?layerCode=507&layerWhere=&xmin=&ymin=&xmax=&ymax=&projection=";
+    const data = await axios.get(url);
+    // console.log(data.data);
+    const features = data.data.features;
+    const streets = [];
+    features.forEach((f) => {
+      console.log(f.attributes);
+      const tmp = f.attributes.shem_angli;
+      if (!streets.includes(tmp)) {
+        streets.push(tmp);
+      }
+    });
+    res.send(streets);
+    console.log(streets);
   } catch (error) {
     console.log(error);
   }
