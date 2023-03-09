d7e54872895fcc37ba66a796e0bce37ea1da1a58
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-03-09 12:13:11 +0200

    added url comment

--- Routes/scraperRoutes.js
+++ Routes/scraperRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express();
-
 const scraperController = require("../Controllers/scraperController");
 
+// http://localhost:8080/scrape/..
 router.get("/TLV-streetWebsite", scraperController.getScarpedStreets);
 router.post("/TLV-StreetsGIS", scraperController.getStreetsFromTLVGis);
 
