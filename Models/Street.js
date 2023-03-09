8ba367587948b4670ba61bf31e9bc13234a8d61f
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-03-09 11:28:56 +0200

    added street model

---
+++ Models/Street.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const streetSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  clean: {
+    type: Number,
+    required: true,
+  },
+  safe: {
+    type: Number,
+    required: true,
+  },
+  scenery: {
+    type: Number,
+    required: true,
+  },
+  accessible: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Street = mongoose.model("Street", streetSchema);
+
+module.exports = Street;
