77a3c84d7ad629762afedea5bb6eb67123c34d12
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-03-10 14:07:37 +0200

    moved mongo to server.js

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const directionsRoute = require("./Routes/directionsRoutes");
 const scraperRoutes = require("./Routes/scraperRoutes");
 app.use(cors());
 app.use(express.json());
 app.use("/api", directionsRoute);
 app.use("/scrape", scraperRoutes);
+
+//connect to db
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log("connected"))
+  .catch((err) => console.log(err));
 
 module.exports = app;
