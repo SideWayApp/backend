8d5c28a93f11878c7c91e2c58db716934fc3c8fb
Yuvalkop22 <Kopelman050@gmail.com>
2023-03-11 17:43:33 +0200

    get streets per alternative

--- Controllers/directionsContorller.js
+++ Controllers/directionsContorller.js
@@ -2,6 +2,47 @@ const axios = require('axios');
 require('dotenv').config();
 
 const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+async function getStreetsInAlternative(index,origin,destination){
+  
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const mode= 'walking';
+  const alternatives= true;
+  const apiUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}&mode=${mode}&alternatives=${alternatives}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const ret = response.data;
+    let streetsPerAlternative = new Set();
+    let arrPerAlt = [];
+    ret.routes[index].legs.map(leg => {
+        leg.steps.map(step=>{
+          const instruction = step.html_instructions;
+          const regex = /<b>(.*?)<\/b>/g;
+          const matches = instruction.match(regex);
+          const arr = matches ? matches.map(match => match.replace(/<\/?b>/g, '')) : [];
+          arr.map(elem => {
+            if (elem.includes("St"))
+              streetsPerAlternative.add(elem)
+              arrPerAlt = Array.from(streetsPerAlternative);
+          });
+        });
+      });
+    //console.log(streetsPerAlternative)
+    return streetsPerAlternative;
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }  
+};
+
+async function getBestAlternative(routes,origin,destination){
+  for (let i =0; i< routes.length; i++){
+    console.log(i);
+    const streets = await getStreetsInAlternative(i,origin,destination);
+    console.log(streets);
+  }
+} 
+
 exports.getDirections = async (req, res) => {
 
   const { origin, destination} = req.body;
@@ -14,29 +55,12 @@ exports.getDirections = async (req, res) => {
   try {
     const response = await axios.get(apiUrl);
     const ret = response.data;
-    let streetsArrayForDirection = [];
-    ret.routes.map(route=>{
-      let streetsPerAlternative = new Set();
-      let arrPerAlt = [];
-      route.legs.map(leg => {
-        leg.steps.map(step=>{
-          const instruction = step.html_instructions;
-          const regex = /<b>(.*?)<\/b>/g;
-          const matches = instruction.match(regex);
-          const arr = matches ? matches.map(match => match.replace(/<\/?b>/g, '')) : [];
-          arr.map(elem => {
-            if (elem.includes("St"))
-              streetsPerAlternative.add(elem)
-              arrPerAlt = Array.from(streetsPerAlternative);
-          });
-        });
-        streetsArrayForDirection.push(arrPerAlt);   
-      });
-    });
-    console.log(streetsArrayForDirection)
-    res.send(streetsArrayForDirection);
+    let routes = ret.routes;
+    const bestAlternativeRoute = getBestAlternative(routes,origin,destination); 
+    res.send(routes);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
   }  
 };
+
