f3aa22ec7bb5247d6a5dcae84c0c246229e7b7ba
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-03-20 11:38:39 +0200

    added map items testing

--- Controllers/mongoMapItemsController.js
+++ Controllers/mongoMapItemsController.js
@@ -2,21 +2,33 @@ const MapItem = require("../Models/MapItem");
 
 // Function to add a new map item
 const addMapItem = async (type, streetName, city) => {
-  const newItem = new MapItem({ type, streetName, city });
-  const result = await newItem.save();
-  return result;
+  try {
+    const newItem = new MapItem({ type, streetName, city });
+    const result = await newItem.save();
+    return result;
+  } catch (err) {
+    throw err;
+  }
 };
 
 // Function to update a map item by its id
 const updateMapItem = async (id, updates) => {
-  const result = await MapItem.findByIdAndUpdate(id, updates, { new: true });
-  return result;
+  try {
+    const result = await MapItem.findByIdAndUpdate(id, updates, { new: true });
+    return result;
+  } catch (err) {
+    throw err;
+  }
 };
 
 // Function to delete a map item by its id
 const deleteMapItem = async (id) => {
-  const result = await MapItem.findByIdAndDelete(id);
-  return result;
+  try {
+    const result = await MapItem.findByIdAndDelete(id);
+    return result;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Function to get a map item by its type
