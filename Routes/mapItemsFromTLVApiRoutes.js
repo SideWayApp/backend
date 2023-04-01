3f165062c0789c9112f1dcffda81b8fdf241841e
Yuvalkop22 <Kopelman050@gmail.com>
2023-04-01 17:53:05 +0300

    fixes in swagger

--- Routes/mapItemsFromTLVApiRoutes.js
+++ Routes/mapItemsFromTLVApiRoutes.js
@@ -6,37 +6,108 @@ const router = express.Router()
 // /**
 // * @swagger
 // * tags:
-// *  name: Cameras Api
-// *  description: Cameras from Tel-Aviv Api
+// *  name: GIS Api
+// *  description: Data from GIS in Tel-Aviv
 // */
 
 /**
  * @swagger
- * /mapitems/allCameras:
+ * /gis/tlv/allLayerCodes:
  *   get:
- *     summary: get all cameras from tlv api and add it to mongodb
- *     tags: [Cameras Api]
+ *     summary: get all layer codes from tlv api
+ *     tags: [GIS Api]
  *     responses:
  *       200:
- *         description: Cameras added to mongodb
- */ router.get(
-	"/allCameras",
-	mapItemsFromTLVApiController.getAllCamerasFromTLVApiAddToMongo
-)
+ *         description: All layer codes
+ */ router.get("/tlv/allLayerCodes",async(req,res)=>{
+	const data = await mapItemsFromTLVApiController.getAllLayersCodes();
+	res.send(data);
+ })
+
 /**
  * @swagger
- * /mapitems/allDangerousConstrucions:
- *   get:
- *     summary: get all dangerous constructions from tlv api and add it to mongodb
- *     tags: [Dangerous Construcions Api]
+ * /gis/tlv/getDataFromLayer/{code}:
+ *   post:
+ *     summary: get data from specific layer by code
+ *     tags: [GIS Api]
+ *     parameters:
+ *       - in: path
+ *         name: code
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The layer code of the layer to get the data
+ *         example: 506
  *     responses:
  *       200:
- *         description: Dangerous constructions added to mongodb
- *
- */
-router.get(
-	"/allDangerousConstrucions",
-	mapItemsFromTLVApiController.getAllDangerousConstructionsFromTLVApiAddToMongo
-)
+ *         description: Data of layer by layer code
+ *       500:
+ *         description: Internal server error.
+ */ router.post("/tlv/getDataFromLayer/:code",async(req,res)=>{
+	const data = await mapItemsFromTLVApiController.getDataFromLayer(req.params.code);
+	res.send(data);
+ })
+
+/**
+ * @swagger
+ * /gis/tlv/getAllItemsByTypeAndCode/{code}/{type}:
+ *   post:
+ *     summary: Get items from a specific layer by code
+ *     tags: [GIS Api]
+ *     parameters:
+ *       - in: path
+ *         name: code
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: "The layer code of the layer to get the data. Example: 506"
+ *       - in: path
+ *         name: type
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: "The type of the layer. Example: camera"
+ *     responses:
+ *       200:
+ *         description: Data of layer by layer code
+ *       500:
+ *         description: Internal server error.
+ */ 
+router.post("/tlv/getAllItemsByTypeAndCode/:code/:type", async(req, res) => {
+  const data = await mapItemsFromTLVApiController.getAllItemsByTypeAndCode(req.params.code, req.params.type);
+  res.send(data);
+});
+
+/**
+ * @swagger
+ * /gis/tlv/addItemsToMongoPerTypeAndCode/{code}/{type}:
+ *   post:
+ *     summary: add items to mongo per type and code
+ *     tags: [GIS Api]
+ *     parameters:
+ *       - in: path
+ *         name: code
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: "The layer code of the layer to get the data. Example: 506"
+ *       - in: path
+ *         name: type
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: "The type of the layer. Example: camera"
+ *     responses:
+ *       200:
+ *         description: the items added to mongodb
+ *       500:
+ *         description: Internal server error.
+ */ 
+router.post("/tlv/addItemsToMongoPerTypeAndCode/:code/:type", async(req, res) => {
+  const data = await mapItemsFromTLVApiController.addItemsToMongoPerTypeAndCode(req.params.code, req.params.type);
+  res.send(data);
+});
+
+
 
 module.exports = router
