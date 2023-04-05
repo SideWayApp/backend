4b683381a8f26c4bd490c0b5138fc97a362ff502
gal levy <gallevy34@gmail.com>
2023-04-05 14:11:00 +0300

    changes in authController, swagger documentation and User Schema
    *register and login fixxed.
    *new documentation for swagger in authRouter
    *added some attributes in Model\User

--- Controllers/authenticationController.js
+++ Controllers/authenticationController.js
@@ -12,13 +12,21 @@ const sendError = (res,code,message)=>{
 const register = async (req,res) => {
     const email = req.body.email
     const password = req.body.password
+    const accessibility = req.body.preferences.accessibility
+    const clean = req.body.preferences.clean
+    const scenery = req.body.preferences.scenery
+    const security = req.body.preferences.security
+    const speed = req.body.preferences.speed
+    const age = req.body.signUpData.age
+    const gender = req.body.signUpData.gender
+    const name = req.body.signUpData.name
 
     try{
         const exists = await User.findOne({'email': email})
         if (exists != null){
             return res.status(400).send({
                 'status':'fail',
-                'error':'not implemented'
+                'error':'this email address is already in use'
             })
         }
         const salt = await bcrypt.genSalt(10)
@@ -26,9 +34,21 @@ const register = async (req,res) => {
 
         const user = User ({
             'email' : email,
-            'password' : hashPassword
+            'password' : hashPassword,
+            'preferences' : {
+                'accessibility' : accessibility,
+                'clean' : clean,
+                'scenery' : scenery,
+                'security' : security,
+                'speed' : speed,
+            }, 
+            'signUpData':{
+                'name': name,
+                'gender' : gender,
+                'age' : age,
+            }
         })
-        const newUser = await user.save()
+        newUser = await user.save()
         res.status(200).send(newUser)
 
     }catch(err){
@@ -43,14 +63,17 @@ const login = async (req,res) => {
     const email = req.body.email
     const password = req.body.password
 
-    if(email == null || password == null)return sendError(res,400,'wrong email or password')
+    if(email == null || password == null) return sendError(res,400,'wrong email or password')
     
     try{
         const user = await User.findOne({'email': email})
-        if(user == null) sendError(res,400,'wrong email or password')
+        if(user == null) return sendError(res,400,'wrong email ')
 
-        const match = bcrypt.compare(password,user.password)
-        if(!match) sendError(res,400,'wrong email or password')
+        const match = await bcrypt.compare(password, user.password)
+        
+        console.log("pass is: " + password)
+        console.log("user pass is: " + user.password)
+        if(!match) return sendError(res,400,'wrong password')
 
         const accessToken = jwt.sign(
             {'id':user._id},
@@ -60,10 +83,8 @@ const login = async (req,res) => {
 
         res.status(200).send({'access':accessToken})
     }catch(err){
-
+        return sendError(res,400,err.message)
     }
-
-   
 };
 
 const logout = async (req,res) => {
--- Models/User.js
+++ Models/User.js
@@ -9,6 +9,42 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  preferences: {
+    accessibility: {
+      type: Boolean,
+      default: false
+    },
+    clean: {
+      type: Boolean,
+      default: false
+    },
+    scenery: {
+      type: Boolean,
+      default: false
+    },
+    security: {
+      type: Boolean,
+      default: false
+    },
+    speed: {
+      type: Boolean,
+      default: false
+    },
+  },
+  signUpData: {
+    name: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+  }
 });
 
 const User = mongoose.model("User", userSchema);
